12aaf92c0af1f08eb2095d2635ce7e1029a2ce93
ishank <ishank.web.dev@gmail.com>
2019-10-27 12:04:03 +0530

    feat: circle and knob added

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure, addDecorator } from "@storybook/react";
+import React from "react";
+import { GlobalStyle } from "../src/shared/global";
+
+// automatically import all files ending in *.stories.js
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+configure(require.context("../src", true, /\.stories\.js$/), module);
